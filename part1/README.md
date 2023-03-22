704a59e03271e940966827c7b66290b40391029a
goatshub <kiatwongwanich@gmail.com>
2023-03-22 14:29:31 +0700

    Add part 1

---
+++ part1/README.md
@@ -0,0 +1,11 @@
+# Part 0 Fundamentals of Web apps
+
+In this part, we will familiarize ourselves with the React-library, which we will be using to write the code that runs in the browser. We will also look at some features of JavaScript that are important for understanding React.
+
+a. [Introduction to React](https://fullstackopen.com/en/part1/introduction_to_react "Introduction to React")
+
+b. [JavaScript](https://fullstackopen.com/en/part1/java_script "JavaScript")
+
+c. [Component state, event handlers](https://fullstackopen.com/en/part1/component_state_event_handlers "Component state, event handlers")
+
+d. [A more complex state, debugging React apps](https://fullstackopen.com/en/part1/a_more_complex_state_debugging_react_apps "A more complex state, debugging React apps")
---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+const Button = ({ text, handleClick }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const StatisticsLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
+
+const Statistics = ({ good, neutral, bad, total }) => {
+  return (
+    <table>
+      <tbody>
+        <StatisticsLine text="good" value={good} />
+        <StatisticsLine text="neutral" value={neutral} />
+        <StatisticsLine text="bad" value={bad} />
+        <StatisticsLine text="all" value={total} />
+        <StatisticsLine text="average" value={(good - bad) / total} />
+        <StatisticsLine text="positive" value={(good / total) * 100 + " %"} />
+      </tbody>
+    </table>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const total = good + neutral + bad;
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button text="good" handleClick={() => setGood(good + 1)} />
+      <Button text="neutral" handleClick={() => setNeutral(neutral + 1)} />
+      <Button text="bad" handleClick={() => setBad(bad + 1)} />
+      <h1>statistics</h1>
+      {!total ? (
+        <div>No feedback given</div>
+      ) : (
+        <Statistics good={good} bad={bad} neutral={neutral} total={total} />
+      )}
+    </div>
+  );
+};
+
+export default App;
